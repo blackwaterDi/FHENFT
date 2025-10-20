f0cea0301901416d32d66084d4ef2c81d6cd7a8a
alexthompson0 <kjgaiqq837@hotmail.com>
2025-10-20 18:13:14 +0800

    fix deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,9 +5,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", { from: deployer, log: true });
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
-
   const deployedFHENFT = await deploy("FHENFT", { from: deployer, log: true });
   console.log(`FHENFT contract: `, deployedFHENFT.address);
 };
