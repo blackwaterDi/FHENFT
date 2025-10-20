f0a162ec987ef455b1f68c76c0c5c77c31a6ebaf
blackwaterDi <fjobpbwrwp1482@hotmail.com>
2025-10-20 18:02:01 +0800

    deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
-    from: deployer,
-    log: true,
-  });
-
+  const deployedFHECounter = await deploy("FHECounter", { from: deployer, log: true });
   console.log(`FHECounter contract: `, deployedFHECounter.address);
+
+  const deployedFHENFT = await deploy("FHENFT", { from: deployer, log: true });
+  console.log(`FHENFT contract: `, deployedFHENFT.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_contracts"; // id required to prevent reexecution
+func.tags = ["FHECounter", "FHENFT"];
